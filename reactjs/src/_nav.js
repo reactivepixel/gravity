10aea0d82a3850b867fbf17aabaf4b3d1819358b
Chapman <Chapman@Apextion.com>
2019-04-19 16:12:14 -0400

    added basic nav

--- reactjs/src/_nav.js
+++ reactjs/src/_nav.js
@@ -1,13 +1,66 @@
 export default {
   items: [
     {
-      name: 'Dashboard',
+      name: 'Marketing Landing',
       url: '/dashboard',
       icon: 'icon-speedometer',
+    },
+
+    {
+      title: true,
+      name: 'Admin',
+      class: '',
+    },
+
+    {
+      name: 'Dashboard',
+      url: '/theme/colors',
+      icon: 'icon-speedometer',
+    },
+
+    {
+      name: 'Clients',
+      url: '/theme/colors',
+      icon: 'icon-people',
+    },
+    {
+      name: '3rd Party Accounts',
+      url: '/theme/colors',
+      icon: 'icon-layers',
+    },
+
+    {
+      title: true,
+      name: 'Client',
+      class: '',
+    },
+
+    {
+      name: 'Dashboard',
+      url: '/theme/colors',
+      icon: 'icon-speedometer',
+    },
+
+    {
+      name: 'Settings',
+      url: '/theme/colors',
+      icon: 'icon-settings',
+    },
+    {
+      name: 'Activity Log',
+      url: '/theme/colors',
+      icon: 'icon-notebook',
       badge: {
-        variant: 'info',
-        text: 'NEW',
+        variant: 'danger',
+        text: '12',
       },
     },
+    {
+      name: 'Invoices',
+      url: '/theme/colors',
+      icon: 'icon-book-open',
+    },
+
+
   ],
 };
